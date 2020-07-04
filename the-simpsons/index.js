ffc2815ffe58c695c8f2241ae52d6e586c49ffef
Iris Mazzuca <irismazzuca@gmail.com>
2020-07-04 20:32:19 -0300

    practica con fetch api - los simpsons

---
+++ the-simpsons/index.js
@@ -0,0 +1,29 @@
+const characterQuote = document.getElementById("characterQuote");
+const characterImage = document.getElementById("characterImage");
+const characterName = document.getElementById("characterName");
+const btn = document.getElementById("btn");
+
+const renderImg = (image) => {
+  characterImage.setAttribute("src", image);
+}
+
+const url = "https://thesimpsonsquoteapi.glitch.me/quotes";
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const newData = data.reduce((accum, currentValue) => {
+      accum = + currentValue;
+    })
+    console.log(newData);
+    renderImg(newData.image);
+    characterQuote.textContent = `"${newData.quote}"`;
+    characterName.textContent = newData.character;
+  })
+
+btn.addEventListener("click", () => {
+  window.location.reload();
+  // history.pushState(url);
+})
